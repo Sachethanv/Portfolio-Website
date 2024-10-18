c7e1a76758e3e5885a587b8bce825ecda7dd3233
Sachethan <Sachethanv@gmail.com>
2024-10-18 18:38:24 +0530

    Used Environment Variables

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 """
 smtplib: This library is used to send emails using the Simple Mail Transfer Protocol (SMTP). 
 It helps connect to email servers and send messages programmatically.
@@ -12,7 +13,7 @@ def send_email(message):#function takes one argument, message, which is the cont
     host =  "smtp.gmail.com" #This is the SMTP server of Gmail, "smtp.gmail.com"
     port=465 #This is the port number used for SMTP over SSL
     username = "sachethanv.codes@gmail.com"#his is the sender’s email address, i.e., the email account you are using to send the email. Here, it’s "sachethanv.codes@gmail.com"
-    password = "cddg twje ldbx prrr"#
+    password = os.getenv("PASSWORD")
     receiver = "sachethanv.codes@gmail.com"#This is the recipient’s email address. In this case, it’s the same as the sender (you are sending the email to yourself: "sachethanv.codes@gmail.com")
     context = ssl.create_default_context()#This creates a secure SSL context, which is necessary for encrypting the connection when you connect to the SMTP server using SSL (port 465).
 
