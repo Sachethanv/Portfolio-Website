71b2e2f2848f3e39fe9cd83d045c1e51b45c80ef
Sachethanv <Sachethanv.codes@gmail.com>
2024-12-26 20:10:36 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -13,7 +13,7 @@ def send_email(message):#function takes one argument, message, which is the cont
     host =  "smtp.gmail.com" #This is the SMTP server of Gmail, "smtp.gmail.com"
     port=465 #This is the port number used for SMTP over SSL
     username = "sachethanv.codes@gmail.com"#his is the sender’s email address, i.e., the email account you are using to send the email. Here, it’s "sachethanv.codes@gmail.com"
-    password = os.getenv("PASSWORD")
+    password = "enxi ldta qniw wevi"
     receiver = "sachethanv.codes@gmail.com"#This is the recipient’s email address. In this case, it’s the same as the sender (you are sending the email to yourself: "sachethanv.codes@gmail.com")
     context = ssl.create_default_context()#This creates a secure SSL context, which is necessary for encrypting the connection when you connect to the SMTP server using SSL (port 465).
 
